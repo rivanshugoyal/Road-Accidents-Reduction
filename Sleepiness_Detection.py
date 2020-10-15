7d6611c85e13ac15701ab0d3f4a2b144b90a0e34
Rivanshu Goyal <goyal419@gmail.com>
2020-10-15 17:37:20 +0530

    Fixed Indentation

--- Sleepiness_Detection.py
+++ Sleepiness_Detection.py
@@ -8,15 +8,15 @@ from imutils import face_utils
 
 def get_EAR(eye):
     """
-	Based on the work by Soukupová and Čech in their 2016 paper,
-	http://vision.fe.uni-lj.si/cvww2016/proceedings/papers/05.pdf
-	"""
-	return (distance.euclidean(eye[1], eye[5])+distance.euclidean(eye[2], eye[4]))/(2*distance.euclidean(eye[0], eye[3]))
+    Based on the work by Soukupová and Čech in their 2016 paper,
+    http://vision.fe.uni-lj.si/cvww2016/proceedings/papers/05.pdf
+    """
+    return (distance.euclidean(eye[1], eye[5])+distance.euclidean(eye[2], eye[4]))/(2*distance.euclidean(eye[0], eye[3]))
 
 def draw_contour(frame, landmark, clr=(0,255,0)):
-	""" Draws contours on the given landmark, green is the default tone"""
-	landmarkHull = cv2.convexHull(landmark)
-	cv2.drawContours(frame, [landmarkHull], -1, clr, 1)
+    """ Draws contours on the given landmark, green is the default tone"""
+    landmarkHull = cv2.convexHull(landmark)
+    cv2.drawContours(frame, [landmarkHull], -1, clr, 1)
 
 def sleepiness_detection():
     """Detects if a person feeling sleepy or not by evaluating EAR value"""
@@ -26,27 +26,27 @@ def sleepiness_detection():
     face_shape_predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-	
+
     stream=cv2.VideoCapture(0)
     frame_counter=0
 
-	while True:
+    while True:
         ret, frame = stream.read()
-		frame = imutils.resize(frame, width=500)
+        frame = imutils.resize(frame, width=500)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		faces = face_detector(gray)
+        faces = face_detector(gray)
 
-		for face in faces:
+        for face in faces:
             face_contour = face_shape_predictor(gray, face)
-			face_contour = face_utils.shape_to_np(face_contour)
+            face_contour = face_utils.shape_to_np(face_contour)
 
-			leftEye, rightEye = face_contour[lStart:lEnd], face_contour[rStart:rEnd]
-			leftEAR, rightEAR = get_EAR(leftEye), get_EAR(rightEye)
-			EAR = (leftEAR + rightEAR) / 2.0
+            leftEye, rightEye = face_contour[lStart:lEnd], face_contour[rStart:rEnd]
+            leftEAR, rightEAR = get_EAR(leftEye), get_EAR(rightEye)
+            EAR = (leftEAR + rightEAR) / 2.0
 
             draw_contour(frame, leftEye)
             draw_contour(frame, rightEye)
-            
+
             if EAR < thresh:
                 frame_counter += 1
                 print(frame_counter)
@@ -55,12 +55,12 @@ def sleepiness_detection():
             else:
                 frame_counter = 0
         cv2.imshow("Main Camera", frame)
-		key = cv2.waitKey(1) & 0xFF
-		if key == ord("q"):
-			break
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
 
-	cv2.destroyAllWindows()
-	stream.stop()
+    cv2.destroyAllWindows()
+    stream.stop()
 
 
 if __name__=='__main__':
