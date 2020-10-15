50be333d6ff149c03e7cea6eeaf765aa61a0a7cd
Rivanshu Goyal <goyal419@gmail.com>
2020-10-15 17:20:12 +0530

    fixed indentation

--- Sleepiness_Detection.py
+++ Sleepiness_Detection.py
@@ -7,7 +7,7 @@ from scipy.spatial import distance
 from imutils import face_utils
 
 def get_EAR(eye):
-	"""
+    """
 	Based on the work by Soukupová and Čech in their 2016 paper,
 	http://vision.fe.uni-lj.si/cvww2016/proceedings/papers/05.pdf
 	"""
@@ -19,44 +19,42 @@ def draw_contour(frame, landmark, clr=(0,255,0)):
 	cv2.drawContours(frame, [landmarkHull], -1, clr, 1)
 
 def sleepiness_detection():
-  """Detects if a person feeling sleepy or not by evaluating EAR value"""
-  face_detector = dlib.get_frontal_face_detector()
-  # .dat file is pre-trained model for face detection
-  # available at http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-  face_shape_predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
-  (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-  (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+    """Detects if a person feeling sleepy or not by evaluating EAR value"""
+    face_detector = dlib.get_frontal_face_detector()
+    # .dat file is pre-trained model for face detection
+    # available at http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+    face_shape_predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 	
-  stream=cv2.VideoCapture(0)
-	frame_counter=0
+    stream=cv2.VideoCapture(0)
+    frame_counter=0
 
 	while True:
-		ret, frame = stream.read()
+        ret, frame = stream.read()
 		frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		faces = face_detector(gray)
 
 		for face in faces:
-			face_contour = face_shape_predictor(gray, face)
+            face_contour = face_shape_predictor(gray, face)
 			face_contour = face_utils.shape_to_np(face_contour)
 
 			leftEye, rightEye = face_contour[lStart:lEnd], face_contour[rStart:rEnd]
 			leftEAR, rightEAR = get_EAR(leftEye), get_EAR(rightEye)
 			EAR = (leftEAR + rightEAR) / 2.0
 
-      draw_contour(frame, leftEye)
-			draw_contour(frame, rightEye)
-
-      if EAR < thresh:
-				frame_counter += 1
-				print(frame_counter)
+            draw_contour(frame, leftEye)
+            draw_contour(frame, rightEye)
+            
+            if EAR < thresh:
+                frame_counter += 1
+                print(frame_counter)
 
         """To DO ALERT DISPLAY"""
-
-			else:
-				frame_counter = 0
-      
-    cv2.imshow("Main Camera", frame)
+            else:
+                frame_counter = 0
+        cv2.imshow("Main Camera", frame)
 		key = cv2.waitKey(1) & 0xFF
 		if key == ord("q"):
 			break
@@ -66,4 +64,4 @@ def sleepiness_detection():
 
 
 if __name__=='__main__':
-  sleepiness_detection()
+    sleepiness_detection()
